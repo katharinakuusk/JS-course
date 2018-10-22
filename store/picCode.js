5eb6411b43519141c991f0057744be9b9866e0ab
Kath <38812348+katharinakuusk@users.noreply.github.com>
2018-10-22 10:49:30 +0300

    Update store/picCode.js

--- store/picCode.js
+++ store/picCode.js
@@ -5,7 +5,7 @@
         images[i].addEventListener("click", function(e){changePicture(e)});
     } 
         
-     var arrows = document.querySelectorAll(".arrows");
+     var arrows = document.querySelectorAll(".arrow");
      
      for (var j = 0; j < arrows.length; j++) {
          arrows[j].addEventListener("click", function(e){arrowsAction(e)});
@@ -16,13 +16,19 @@ function changePicture(eventObj) {
         var largeBlock = document.querySelector(".large-picture");
         largeBlock.innerHTML = " ";
         var eventElement = eventObj.target;
-        var imagesName = eventElement.id.split("_");
-        var src = "largeGalleryPic/" + imagesName[1] + ".jpeg";
-        var imageDomElement = document.createElement("img");
-        imageDomElement.src = src;
-        imageDomElement.id = imagesName[1];
-        imageDomElement.addEventListener("error", function(e){unloadCase(e)});
-        largeBlock.appendChild(imageDomElement);
+        showPicture(eventElement);
+}
+
+function showPicture(eventElement) { //проблема в том, что мы со стрелок сюда передаем число, а с изменения картинок передаем объект, нужно приравнять объект к числу и передавать сюда число или снизу передавать объект, но это я пока не поняла как. 
+    var largeBlock = document.querySelector(".large-picture");
+    
+    var imagesName = eventElement.id.split("_");
+    var src = "largeGalleryPic/" + imagesName[1] + ".jpeg";
+    var imageDomElement = document.createElement("img");
+    imageDomElement.src = src;
+    imageDomElement.id = imagesName[1];
+    imageDomElement.addEventListener("error", function(e){unloadCase(e)});
+    largeBlock.appendChild(imageDomElement);
 }
                                          
 function unloadCase(e) {
@@ -34,12 +40,19 @@ function arrowsAction(e) {
     var eventArrow = e.target;
     var eventArrowType = eventArrow.id.split("-");
     var picBlock = document.querySelector(".large-picture");
-    var activeImage = picBlock.childNodes;
+    console.log("picBlock.innerHTML", picBlock.innerHTML);
+    var activeImage = document.querySelector("img");
+    var activeImageId = parseInt(activeImage.id);
+    var nextImageId = 0;
+    var nextImage;
     if (eventArrowType[1] == "right") {
-        var nextImageId = ++parseInt(activeImage.id);
-        changePicture(eventObj);
+        nextImageId = ++activeImageId;
+        nextImage = document.getElementById("sm_" + nextImageId);
+        showPicture(nextImage);
     } else if (eventArrowType[1] == "left") {
-        var nextImageId = --parseInt(activeImage.id);
+        nextImageId = --activeImageId;
+        nextImage = document.getElementById("sm_" + nextImageId);
+        showPicture(nextImage);
     } else {
         
     }
